771efae251200910de1ac7c2ee9a59fa531fbad8
binhle410 <binh@sunrise.vn>
2020-05-11 08:51:32 +0700

    - Fck

--- libraries/ngx/dmc-argon/src/app/layouts/admin-layout/admin-layout.component.ts
+++ libraries/ngx/dmc-argon/src/app/layouts/admin-layout/admin-layout.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {SwUpdate} from "@angular/service-worker";
 
 @Component({
@@ -9,6 +9,16 @@ import {SwUpdate} from "@angular/service-worker";
 export class AdminLayoutComponent implements OnInit {
 
   isUpdateMsgVisible = false;
+  deferredPrompt: any;
+  isA2HSButtonVisible = false;
+
+  @HostListener('window:beforeinstallprompt', ['$event'])
+  onBeforeInstallPrompt(e) {
+    console.log(e);
+    e.preventDefault;
+    this.deferredPrompt = e;
+    this.isA2HSButtonVisible = true;
+  }
 
   constructor(private swUpdate: SwUpdate) {
 
@@ -20,11 +30,30 @@ export class AdminLayoutComponent implements OnInit {
     })
   }
 
+  closeA2HSMessage() {
+    this.isA2HSButtonVisible = false;
+  }
+
   closeUpdateMsg() {
     this.isUpdateMsgVisible = false;
   }
 
   reload() {
     this.swUpdate.activateUpdate().then(() => document.location.reload());
+  }
+
+  add2HS() {
+    this.isA2HSButtonVisible = false;
+    this.deferredPrompt.prompt();
+    this.deferredPrompt.userChoice.then((choiceResult) => {
+      if (choiceResult.outcome === 'accepted') {
+        console.log('User accepted A2HS prompt');
+        localStorage.setItem('a2hs', JSON.stringify(choiceResult));
+      } else {
+        console.log('User refused A2HS prompt');
+        localStorage.setItem('a2hs', JSON.stringify(choiceResult));
+      }
+      this.deferredPrompt = null;
+    });
   }
 }
